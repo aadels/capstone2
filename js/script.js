840be6b6acd16b95c4b337d708159073f3372f71
Ann Adelsberger <adelsbergerann@gmail.com>
2015-03-07 22:27:17 -0500

    update map

--- js/script.js
+++ js/script.js
@@ -305,95 +305,58 @@ $(function() {
 
 });
 
-    // redraw map on resize
-    $(window).resize(function(){
-      location.reload();
-    });
+//redraw map on resize
+$(window).resize(function(){
+  location.reload();
+});
+//add tour stops
+  var locations = [
+    ['Union United', 42.381307, -71.099733, 10, 'http://player.vimeo.com/video/111527649'],
+    ['Affordable Housing', 42.380508, -71.100172, 9,'http://player.vimeo.com/video/111537719'],
+    ['Local Jobs and Workers Rights', 42.380746,    -71.098092, 8,'http://player.vimeo.com/video/111542966'],
+    ['Small Business Protection', 42.38011, -71.097088, 7,'http://player.vimeo.com/video/111544069'],
+    ['History of Community Organizing', 42.379674, -71.096366, 6,'http://player.vimeo.com/video/111551065'],
+    ['Green Space', 42.377, -71.096093, 5,'http://player.vimeo.com/video/111554655'],
+    ['Green Line Extension', 42.376768, -71.095627, 4,'http://player.vimeo.com/video/111555617'],
+    ['Public Safety', 42.378882, -71.092666, 3,'http://player.vimeo.com/video/111558282'],
+    ['Arts, Culture and Community Resources', 42.379684, -71.093669, 2,'http://player.vimeo.com/video/111529297'],
+    ['Participatory Planning', 42.379332, -71.094231, 1,'http://player.vimeo.com/video/111531551']
+  ];
+//Draw Map
+  google.maps.event.addDomListener(window, 'load', init);
+  function init() {
 
-// Create and Initialise the Map (required) our google map below
-
-    google.maps.event.addDomListener(window, 'load', init);
-
-    function init() {
-        // Basic options for a simple Google Map
-        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-
+//set map options
     var mapOptions = {
-
-         // How zoomed in you want the map to start at (always required)
-
       zoom: 14,
       scrollwheel: false,
-      // The latitude and longitude to center the map (always required)
-
-      center: new google.maps.LatLng(42.379198, -71.094261), // Nova Scotia
-
-      // How you would like to style the map. 
-      // This is where you would paste any style found on [Snazzy Maps][1].
-      // copy the Styles from Snazzy maps,  and paste that style info after the word "styles:"
-
-        styles: [{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"saturation":"-1"},{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"administrative","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.locality","elementType":"geometry","stylers":[{"visibility":"off"},{"saturation":"1"}]},{"featureType":"administrative.neighborhood","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"administrative.neighborhood","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"visibility":"off"},{"saturation":"28"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#66a8b8"},{"visibility":"on"}]}]
+      center: new google.maps.LatLng(42.379198, -71.094261), // Union Square, Somerville, MA
+      
+//set map styles
+      styles: [{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"saturation":"-1"},{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"administrative","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.locality","elementType":"geometry","stylers":[{"visibility":"off"},{"saturation":"1"}]},{"featureType":"administrative.neighborhood","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"administrative.neighborhood","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"visibility":"off"},{"saturation":"28"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#66a8b8"},{"visibility":"on"}]}]
     };
 
 
 
+   
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          window.location.href = this.url;
+        }
+      })(marker, i));  
+    
     var mapElement = document.getElementById('map1');
 
-      // Create the Google Map using out element and options defined above
-      var map = new google.maps.Map(mapElement, mapOptions);
+      // Create the Google Map as defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+  }
 
-    // Following section, you can create your info window content using html markup
-
-      var contentString = '<div id="content">'+
-          '<div id="siteNotice">'+
-          '</div>'+
-          '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-          '<div id="bodyContent">'+
-          '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-          'sandstone rock formation in the southern part of the '+
-          'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-          'south west of the nearest large town, Alice Springs; 450&#160;km '+
-          '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-          'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-          'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-          'Aboriginal people of the area. It has many springs, waterholes, '+
-          'rock caves and ancient paintings. Uluru is listed as a World '+
-          'Heritage Site.</p>'+
-          '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-          'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-          '(last visited June 22, 2009).</p>'+
-          '</div>'+
-          '</div>';
-
-
-    // Define the image to use for the map marker (58 x 44 px)
-
-      var image = 'http://uploads.webflow.com/537f700d5bb0a27f34444d0c/53b054015eb95f024f4d7c5e_map_marker.png';
-
-    // Define the Lattitude and Longitude for the map location
-
-      var myLatLng = new google.maps.LatLng(42.379198, -71.094261); 
-
-    // Define the map marker characteristics
-
-      var mapMarker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: image,
-      title:  'Tour of Union Square'
-
-      });
-
-     // Following Lines are needed if you use the Info Window to display content when map marker is clicked
-
-     var infowindow = new google.maps.InfoWindow({
-                          content: contentString
-                      });
-
-    // Following line turns the marker, into a clickable button and when clicked, opens the info window
-
-          google.maps.event.addListener(mapMarker, 'click', function() {
-              infowindow.open(map, mapMarker);
-          });  
-
-}
+};
